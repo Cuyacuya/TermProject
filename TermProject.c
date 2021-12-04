c72515f45bab660c837fd42183ca41b533116e0f
CUYA <vkcclsrn156@naver.com>
2021-12-05 01:36:00 +0900

    드디어 제대로 됨. 이제 오류 정리하면될듯 selection 부분이랑 관리자 모드 부분

--- TermProject.c
+++ TermProject.c
@@ -1,7 +1,7 @@
 //TermProject - Vending Machine
 #define _CRT_SECURE_NO_WARNINGS
-//#define _CRT_SECURE_NO_DEPRECATE  
-//#define _CRT_NONSTDC_NO_DEPRECATE
+#define _CRT_SECURE_NO_DEPRECATE  
+#define _CRT_NONSTDC_NO_DEPRECATE
 #include <stdio.h>
 #include <conio.h>
 #include <Windows.h>
@@ -14,8 +14,8 @@ int money = 0; //투입한 금액
 int change = 0; //잔돈
 int selection = 0; //선택 
 int sales = 0; //전체 매출
-int option = 0; //관리자모드에서 옵션값
-int YN = 0; //마지막 result에 대한 값
+char option = -1; //관리자모드에서 옵션값
+char YN = 'A'; //마지막 result에 대한 값
 
 
 void title() {
@@ -40,14 +40,16 @@ void manual(int money) {
     printf("================================\n");
     printf("====[상품을 선택 해 주세요.]====\n");
 }
-void selectItem(int selection,int money) {
-    if (item[selection] < 0) {
+int selectItem(int selection,int money) {
+    if (item[selection] <= 0) {
         printf("재고 없음");
         printf("다시 선택해 주세요.");
+        return 0;
     }
     else if (money < price[selection]) {
         printf("돈이 부족합니다.");
         printf("다시 선택해 주세요.");
+        return 0;
     }
     else {
         change = money - price[selection]; //잔돈계산
@@ -56,18 +58,7 @@ void selectItem(int selection,int money) {
         return change, sales;
     }
 }
-/*
-// 사용할지 말지 고민중...
-//사용하면 selectItem에서 slection과 money만 사용하면 되므로 main에서 opctin()을 사용시 selection만 적어주면됨. or selection과 sales둘다 적어줘야함 -> 사용해보기로!
-int payment(int money, int selection, int change, int sales) {
-    change = money - price[selection];
-    sales += price[selection];
-    item[selection] -= 1;
-    return change, sales;
-}
-*/
-
-void admin() {
+int admin() {
     printf("=========[ 관리자 모드 ]=======\n");
     printf("================================\n");
     printf("========[ 판매 매출 : W ]=======\n");
@@ -75,20 +66,25 @@ void admin() {
     printf("=======[ 자판기 종료 : X ]======\n");
     printf("================================\n");
     printf("======[옵션을 선택해주세요]=====\n");
-    scanf("%c", &option);
+    while (1) {
+        scanf(" %c", &option);
         if (option == 'W' || option == 'w') {
             printf("판매 매출 : %d원", sales);
+            return 0;
         }
         else if (option == 'E' || option == 'e') {
             sales = 0;
             printf("판매 매출이 초기화 되었습니다.");
+            return 0;
         }
-        return option;
+        else printf("잘못된 옵션입니다. 다시 선택해주세요.");
+    }
 }
 //함수에 대해 공부
 void result(int selection) {
+    int i = selection;
     printf("================================\n");
-    printf("=[주문하신 상품 %s 나왔습니다.]=\n", itemN[selection]);
+    printf("=[주문하신 상품 %s 나왔습니다.]=\n", itemN[i]);
     printf("================================\n");
     printf("=======[ 계속 뽑기 : Y ]========\n");
     printf("=======[ 잔돈 받기 : N ]========\n");
@@ -104,7 +100,7 @@ int main()
         manual(money);
         scanf("%d", &selection);
         if (selection == 0) {
-            printf("관리자 모드를 실행합니다.");
+            printf("관리자 모드를 실행합니다.\n");
             admin();
             if (option == 'X' || option == 'x') {
                 printf("자판기를 종료합니다.");
@@ -115,44 +111,20 @@ int main()
             selection = selection - 1; //상품 번호와 인덱스가 동일하게 변경.
             selectItem(selection, money);
             result(selection);
-            scanf("%c", &YN);
+            scanf(" %c", &YN);
             if (YN == 'N' || YN == 'n') {
-                printf("%잔돈 : d원입니다.\n", change);
+                printf("잔돈 : %d원입니다.\n", change);
                 printf("이용해주셔서 감사합니다.");
                 return 0;//자판기를 종료
             }
-            else if (YN == 'Y' || YN == 'y') {
+            if (YN == 'Y' || YN == 'y') {
                 continue; //자판기를 계속 이용
             }
         }
-        /*
-        if (0 <= selection <= 10) {
-            selection = selection - 1; //상품 번호와 인덱스가 동일하게 변경.
-            selectItem(selection, money);
-            result(selection);
-            scanf("%d", &YN);
-            if (YN == 'N' || YN == 'n') {
-                printf("잔돈 %d원 입니다.", change);
-                return 0;
-            }
-            else if (YN == 'Y' || YN == 'y') {
-                printf("감사합니다. 다음에 또 이용해주세요");
-                return 0;
-            }
-        }
-        else if (selection == 'W' || selection == 'w' || selection == 'E' || selection == 'e' || selection == 'Q' || selection == 'q') {
-            option(sales, selection);
-            return 0;
-        }
-        else if (selection == 'X' || selection == 'x') {
-            printf("자판기 전원이 꺼집니다.");
-            return 0;
-        }
         else {
-            printf("잘못된 입력입니다.\n다시 선택해 주십시오");
-            selectItem(selection, money);
+            printf("잘못된 선택입니다.");
+            return 0;
         }
-        */
     }
     return 0;
 }
